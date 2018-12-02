3312d2ff09230ca5cbfde2d0978c97a03233affd
Chris Gaona <thebeloved88@sbcglobal.net>
2018-12-01 19:51:11 -0800

    Finishes exercise 7 for playing with animations

--- Exercise7/index.js
+++ Exercise7/index.js
@@ -7,19 +7,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  circle: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    borderRadius: 100,
+  }
 });
 
 class Challenge7 extends Component {
-  state = {}
+  state = {
+    radius: new Animated.Value(0),
+  }
 
   handlePress = () => {
+    this.state.radius.setValue(0);
+    Animated.timing(this.state.radius, {
+      toValue: 200,
+      duration: 500,
+    }).start();
   }
 
   render() {
+    const opacity = this.state.radius.interpolate({
+      inputRange: [0, 200],
+      outputRange: [1, 0],
+    })
+
     return (
       <TouchableOpacity style={styles.container} onPress={this.handlePress}>
         <SafeAreaView style={styles.container}>
-
+          <Animated.View
+            style={[
+              styles.circle,
+              {
+                width: this.state.radius,
+                height: this.state.radius,
+                opacity,
+              }
+            ]}
+          />
         </SafeAreaView>
       </TouchableOpacity>
     );
