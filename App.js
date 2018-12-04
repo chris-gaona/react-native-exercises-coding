f0bf26432c48c766d4742a3578c144fa0c488222
Chris Gaona <thebeloved88@sbcglobal.net>
2018-12-03 21:28:09 -0800

    Finishes exercise 8; complex react-navigation

--- App.js
+++ App.js
@@ -4,8 +4,8 @@
 // import App from './Exercise4';
 // import App from './Exercise5';
 // import App from './Exercise6';
-import App from './Exercise7';
-// import App from './Exercise8';
+// import App from './Exercise7';
+import App from './Exercise8';
 // import App from './Exercise9';
 // import App from './Exercise10';
 
--- Exercise8/index.js
+++ Exercise8/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Button } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer, createDrawerNavigator } from "react-navigation";
+import { Ionicons } from '@expo/vector-icons'
 
 const GenericScreen = ({ navigation, title, Name }) => (
   <View style={{ flex: 1 }}>
@@ -14,4 +16,117 @@ const GenericScreen = ({ navigation, title, Name }) => (
   </View>
 );
 
-export default () => null;
+const GenericStack = (Name, title) => (
+  createStackNavigator({
+    [Name]: {
+      screen: (props) => <GenericScreen {...props} title={title} Name={Name} />,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: title,
+        headerLeft: (
+          <Button
+            title="Drawer"
+            onPress={() => navigation.openDrawer()}
+          />
+        )
+      }),
+    },
+    [`${Name}+1`]: {
+      screen: View,
+      navigationOptions: {
+        headerTitle: `${title}+1`
+      },
+    },
+  })
+)
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: GenericStack('Home', 'Home'),
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-home" size={28} color={tintColor} />
+      )
+    }
+  },
+  Browse: {
+    screen: GenericStack('Browse', 'Browse'),
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-browsers" size={28} color={tintColor} />
+      )
+    }
+  },
+  Search: {
+    screen: GenericStack('Search', 'Search'),
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-search" size={28} color={tintColor} />
+      )
+    }
+  },
+  Radio: {
+    screen: GenericStack('Radio', 'Radio'),
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-radio" size={28} color={tintColor} />
+      )
+    }
+  },
+});
+
+const DrawerNavigator = createDrawerNavigator({
+  Home: TabNavigator,
+  'Your Daily Mix': {
+    screen: GenericStack('Your Daily Mix', 'Your Daily Mix')
+  },
+  'Recently Played': {
+    screen: GenericStack('Recently Played', 'Recently Played')
+  },
+  Songs: {
+    screen: GenericStack('Songs', 'Songs')
+  },
+  Albums: {
+    screen: GenericStack('Albums', 'Albums')
+  },
+  Artists: {
+    screen: GenericStack('Artists', 'Artists')
+  },
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: View,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Settings',
+      headerRight: (
+        <Button
+          title="Close"
+          onPress={() => navigation.goBack(null)}
+        />
+      )
+    }),
+  },
+});
+
+const RootNavigator = createStackNavigator({
+  Primary: {
+    screen: DrawerNavigator,
+  },
+  Setting: {
+    screen: SettingsStack,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Settings',
+      headerRight: (
+        <Button
+          title="Close"
+          onPress={() => navigation.goBack(null)}
+        />
+      )
+    })
+  }
+}, {
+  mode: 'modal',
+  headerMode: 'none',
+})
+
+export default createAppContainer(RootNavigator);
